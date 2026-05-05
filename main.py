0b82a2c7826b286316d7106b2e2d60ea91b3c0d9
Evan Ngo <mr.evanngo@gmail.com>
2026-05-05 16:42:57 -0700

    Fix lint errors

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import os          # unused import (F401)
-import sys         # unused import (F401)
-x=1+2             # missing whitespace (E225)
-y = [1,2,3,4,5]  # missing whitespace after comma (E231)
 
 def main():
     print("Hello from 190!")
